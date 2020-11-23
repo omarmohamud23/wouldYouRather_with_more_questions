a683a92c51e72c54ad232fd1932c1e777083a46b
Omar <Omar>
2020-11-23 12:45:52 -0600

    Would you rather with more questions

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Would You Rather??</h1>
+
+      <would-you-rather-with-more-components 
+       v-for="question in questions"
+       v-bind:question="question.wyrQuestion"
+       v-bind:answer1="question.wyrAnswer1"
+      v-bind:answer2="question. wyrAnswer2"
+       v-bind:id = "question.id "
+      v-on:answer-changed="answerChanged">
+      </would-you-rather-with-more-components>
+
+      <p>You Would Rather...</p>
+
+      <ul v-for="answer in answers" > 
+        <li>{{ answer }}</li>
+      </ul>
+
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRatherWithMoreComponents from './components/WouldYouRatherWithMoreComponents.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRatherWithMoreComponents
+  },
+  data () {
+    return {
+      questions: [
+     
+     {
+       id: 0,
+       wyrQuestion: 'Would you rather be able to control fire or water?',
+      wyrAnswer1: 'Fire',
+      wyrAnswer2: 'Water',
+      userSelectionMessage: ''
+     },
+     {
+       id:1,
+       wyrQuestion: 'Would you rather be really good at math or really good at sports?' ,
+       wyrAnswer1: 'Math',
+       wyrAnswer2: 'Sports',
+       userSelectionMessage: ''
+     },
+    {
+      id:2,
+      wyrQuestion: 'Would you rather meet your favorite celebrity or be on a TV show?' ,
+     wyrAnswer1: 'Favorite celebrity',
+     wyrAnswer2: 'Tv show',
+     userSelectionMessage: '' 
+    }  
+      ]
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+     this.userSelectionMessage = `Thanks! You chose ${choice}`
+    }
   }
 }
 </script>
@@ -24,5 +73,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  background-color: aquamarine;
+  border:blue
 }
 </style>
